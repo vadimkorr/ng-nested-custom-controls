42bc2df43ff0bb0297567ac8cc6eccfab082b3e8
Vadim <korobeinikovv@mail.ru>
2018-03-22 21:26:23 +0300

    Added additional packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
